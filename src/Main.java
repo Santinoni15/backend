bbf33a7570e7b43d873dc20360baaf14c072ef52
ana <anajuliasantinoni15@gmail.com>
2024-03-21 08:16:04 -0300

    Exerc2

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,9 @@
-package org.example;
-
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        String url = "https://venson.net.br/resources/data/nome.txt";
-        //cria um cliente http
-        HttpClient client = HttpClient.newHttpClient();
-        //cria uma requisicao http
-        HttpRequest requisicao = HttpRequest.newBuilder().uri(URI.create(url)).build();
-        //executa requisicao e recebe resposta
-        HttpResponse<String> resposta = client.send(requisicao, HttpResponse.BodyHandlers.ofString());
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        Jogador jogador = new Jogador();
+        System.out.println(jogador.getDescricao());
+
     }
+
 }
